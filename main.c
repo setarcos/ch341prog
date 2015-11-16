2bc5f42025470758b56ba9ecf3ccaa517db1a2d5
persmule <persmule@gmail.com>
2015-11-16 20:52:31 +0800

    Add ability to configure the bus speed.

--- main.c
+++ main.c
@@ -35,6 +35,7 @@ int main(int argc, char* argv[])
     int cap;
     int length = 0;
     char op = 0;
+    uint32_t speed = CH341A_STM_I2C_20K;
     int8_t c;
 
     const char usage[] =
@@ -44,18 +45,22 @@ int main(int argc, char* argv[])
         " -e, --erase            erase the entire chip\n"\
         " -l, --length <bytes>   manually set length\n"\
         " -w, --write <filename> write chip with data from filename\n"\
-        " -r, --read <filename>  read chip and save data to filename\n";
+        " -r, --read <filename>  read chip and save data to filename\n"\
+        " -t, --turbo            increase the i2c bus speed (-tt to use much faster speed)\n"\
+	" -d, --double           double the spi bus speed\n";
     const struct option options[] = {
         {"help",    no_argument,        0, 'h'},
         {"erase",   no_argument,        0, 'e'},
         {"write",   required_argument,  0, 'w'},
         {"length",   required_argument,  0, 'l'},
         {"read",    required_argument,  0, 'r'},
+	{"turbo",   no_argument,  0, 't'},
+	{"double",  no_argument,  0, 'd'},
         {0, 0, 0, 0}};
 
         int32_t optidx = 0;
 
-        while ((c = getopt_long(argc, argv, "hiew:r:l:", options, &optidx)) != -1){
+        while ((c = getopt_long(argc, argv, "hiew:r:l:td", options, &optidx)) != -1){
             switch (c) {
                 case 'i':
                 case 'e':
@@ -76,6 +81,14 @@ int main(int argc, char* argv[])
 		case 'l':
 		    length = atoi(optarg);
 		    break;
+		case 't':
+		    if ((speed & 3) < 3) {
+		        speed++;
+		    }
+		    break;
+		case 'd':
+		    speed |= CH341A_STM_SPI_DBL;
+		    break;
                 default:
                     printf("%s\n", usage);
                     return 0;
@@ -92,7 +105,7 @@ int main(int argc, char* argv[])
     ret = ch341Configure(CH341A_USB_VENDOR, CH341A_USB_PRODUCT);
     if (ret < 0)
         return -1;
-    ret = ch341SetStream(CH341A_STM_I2C_100K);
+    ret = ch341SetStream(speed);
     if (ret < 0) goto out;
     ret = ch341SpiCapacity();
     if (ret < 0) goto out;
