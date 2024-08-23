019b470486386170456befbbff2f211f843df702
Diratof Azdimarlow <you@example.com>
2024-08-24 02:57:47 +0500

    flushed garbage

--- flake.nix
+++ flake.nix
@@ -2,23 +2,13 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
-  outputs = { self, nixpkgs, ... }: let
+  outputs = { nixpkgs, ... }: let
     pkgs = import nixpkgs {
       system = "x86_64-linux";
     };
   in {
     devShells."x86_64-linux".default = pkgs.mkShell {
-      nativeBuildInputs = with pkgs; let 
-        fixWrapper = pkgs.runCommand "fix-wrapper" {} ''
-          mkdir -p $out/bin
-          for i in ${pkgs.gcc.cc}/bin/*-gnu-gcc*; do
-            ln -s ${pkgs.gcc}/bin/gcc $out/bin/$(basename "$i")
-          done
-          for i in ${pkgs.gcc.cc}/bin/*-gnu-{g++,c++}*; do
-            ln -s ${pkgs.gcc}/bin/g++ $out/bin/$(basename "$i")
-          done
-        '';
-      in[
+      nativeBuildInputs = with pkgs; [
         gnumake pkg-config systemDlibs libusb1
       ];
       hardeningDisable = [ "all" ];
@@ -26,7 +16,7 @@
     packages."x86_64-linux".default = pkgs.stdenv.mkDerivation {
       name = "ch341prog";
       pname = "ch341prog";
-      version = "ch341prog";
+      version = "9999";
       src = ./.;
       buildInputs = with pkgs; [
         gcc gnumake libusb1 systemdLibs
